b8a29132e4ce64a48b34194cc407e0cd83ee5a51
natandaniel <natan.daniel0@gmail.com>
2025-02-21 17:24:05 +0100

    reinitializing project with spring initializr

---
+++ src/test/java/org/nd/online_learning_platform/OnlineCoursePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nd.online_learning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoursePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
